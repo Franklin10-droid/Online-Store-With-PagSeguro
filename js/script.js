b89635a7e140dc03f521ee8f0576a7fe8ae9d81c
Jhonata Vinicius Da Silva Araujo <52162636+jhony2488@users.noreply.github.com>
2020-01-19 10:47:10 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var visualizarCarrinho = document.querySelector('div #carrinho-visu iframe');
+var alertInstrucoes = document.getElementById('avisoInstrucoes');
+setInterval(function() { carregarPag(); }, 12000);
+var pagSeguroCompra = "https://pagseguro.uol.com.br/checkout/checkout.jhtml";
+
+function carregarPag() {
+    visualizarCarrinho.src = "https://pagseguro.uol.com.br/v2/checkout/cart.html?action=view";
+}
+
+function openWindow() {
+    myWindow = window.open("https://pagseguro.uol.com.br/checkout/checkout.jhtml", "_blank");
+}
+
+function closePag() {
+    myWindow.close();
+    window.frames.closewindow();
+}
+
+function fecharAviso() {
+    alertInstrucoes.style.display = "none";
+}
