97e99892eea168ff4e271d65133135842d55f849
Jhonata Vinicius Da Silva Araujo <52162636+jhony2488@users.noreply.github.com>
2020-01-22 13:29:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+/*
+Autor: Jhonata Vinicius
+*/
+
 var visualizarCarrinho = document.querySelector('div #carrinho-visu iframe');
 var alertInstrucoes = document.getElementById('avisoInstrucoes');
 setInterval(function() { carregarPag(); }, 12000);
